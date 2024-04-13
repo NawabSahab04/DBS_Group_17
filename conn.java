4cc1f81b98660640d13e2e8a449ca09b2d7e7f76
virendra <virendramane745@gmail.com>
2024-04-13 14:20:50 +0530

    Hello

--- conn.java
+++ conn.java
@@ -6,8 +6,11 @@ public class conn {
 
     public conn(){
         try{
-            Class.forName("ojdbc11.oracle.jdbc.driver.OracleDriver");
-            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectigi","root","root");
+            // Load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Establish connection to your MySQL database
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/PROJECTIGIFinal","root","root");
+            // Create a statement object
             s = c.createStatement();
         }
         catch(Exception e){
